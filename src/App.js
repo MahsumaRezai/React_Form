981044d5f53857ac3811215adf4ce13ffe3bb288
Masoumeh Rezai <you@example.com>
2023-05-27 09:00:51 +0430

    work on haed section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-
+ 
 const App = () => {
   return (
     <div>
-      <h1>hello</h1>
 
     </div>
   );
---
+++ src/componets/UI/Head.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import classes from 'react';
+
+const Card = () => {
+    <Fragment>
+        <div className={classes.head}>
+
+        </div>
+
+    </Fragment>
+
+}
+export default Card;
