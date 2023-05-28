16f8118cb51f08f28ae3e61a3cd71cc0fd000aab
Masoumeh Rezai <you@example.com>
2023-05-28 08:37:54 +0430

    word on data save

--- src/Componet/Form.js
+++ src/Componet/Form.js
@@ -1,18 +1,29 @@
-import { Fragment } from "react";
-import classes from './Form.module.css'; 
+import { Fragment, useState } from "react";
+import classes from './Form.module.css';
 import Button from "./Button";
 
 const Form = () => {
+    const [nameEnter, nameFun] = useState('');
+    const [passwordEnter, passwordFun] = useState('');
+    const datanameHandler = (event) => {
+        nameFun(event.target.value);
+        console.log(nameEnter)
+
+    }
+    const datapasswordHandler = (event) => {
+        passwordFun(event.target.value);
+        console.log(passwordEnter)
+    }
     return (
         <Fragment>
             <form className={classes.form}>
                 <div className={classes.content}>
                     <label>Name</label>
-                    <input type="text"></input>
+                    <input type="text" onChange={datanameHandler}></input>
                 </div>
                 <div className={classes.content_one}>
                     <label>Password</label>
-                    <input type="password"></input>
+                    <input type="password" onChange={datapasswordHandler}></input>
                 </div>
                 <div>
                     <Button>Login</Button>
